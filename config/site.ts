1d7032fe1d671bcbf23adf1745c51a4ff66457cd
rlphrazz <rlphrazz@gmail.com>
2024-02-20 00:24:02 -0500

    First Commit

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+	name: "Demo",
+	url: "",
+	ogImage: "",
+	description: "Beautifully designed components built with Radix UI and Tailwind CSS.",
+	links: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/ralphrazzouk/demo",
+	},
+};
+
+export type SiteConfig = typeof siteConfig;
